fab4f053feb068b20213fd01f80f9b0192eaa7ef
Omegalpha44 <charlesduval44@gmail.com>
2021-08-17 09:55:42 +0200

    rajout d'un fichier rééponfant à la question en utilisant le machine learning. Le programme tourne mais le résultat n'est pas encore assuré.

--- deepl.py
+++ deepl.py
@@ -1,18 +1,41 @@
+import os
+from numpy.lib.nanfunctions import nancumsum
 import tensorflow as tf
 from icecream import ic
 import numpy
 from tensorflow.python.keras import activations
 from tensorflow.python.keras.engine import input_layer
+import pandas
 
 # Présentation du modèle : on prend en entrée un vecteur à 5 dimensions, chacune représentant une information nécessaire pour résoudre le problème
 #ensuite, on le fait passer dans la suite de neurone (10 neurones pour l'instant)
 #enfin, on demande le trie pour savoir cb de
 
-model = tf.keras.Sequential([
-    tf.keras.layers.InputLayer(5), # 5 en entrée, dans notre tenseur : le pays, la pop totale, le nb d'accidenté au total, si présence de véhicule et la loi
-    tf.keras.layers.Dense(10,activation='relu'),
-    tf.keras.layers.Dense(5,activation="relu") # homme (femme obtenu par calcul), cycliste touché, tête, visage, neck
-]
-)
+model = tf.keras.Sequential(
+    [
+    tf.keras.layers.Dense(100), # 5 en entrée, dans notre tenseur. Cf .csv pour se référer aux différentes caractéristiques
+    tf.keras.layers.Dense(60,activation='relu'), # la couche du milieu
+    tf.keras.layers.Dense(30,activation="relu"),
+    tf.keras.layers.Dense(5,activation="relu")
+    ])
+column_names = ["début d'étude" , "fin d'étude" , "nombre de véhicule" , "pays" , "loi" , "population" , "accidenté" , "portait un casque" , "tête" , "nuque" , "visage"]
+
+data_train = pandas.read_csv("valeur.csv",names = column_names)
+data_features = data_train.copy()
+label_names = column_names[6:]
+data_labels = pandas.read_csv("label.csv",names = label_names)
+ic( data_features)
+ic( data_labels)
+data_features = numpy.array(data_features)
+data_labels = numpy.array(data_labels)
+
+data_features = numpy.asarray(data_features).astype(numpy.float32)
+
+data_labels = numpy.asarray(data_labels).astype(numpy.float32)
 
 
+
+model.compile(loss=tf.losses.MeanSquaredError(),
+                optimizer=tf.optimizers.Adam())
+
+model.fit(data_features,data_labels,epochs=1000)
